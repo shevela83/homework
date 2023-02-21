b871f125da0e7d58be52f5b401c5dc0ad272a145
Shevela_Denis <shevela83@mail.ru>
2023-02-21 13:39:12 +0300

    hw03

---
+++ homework03/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+при N < 0, нужно вывести от N до -1
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+/*Console.WriteLine("Введите число ");
+double x = double.Parse(Console.ReadLine());
+double y = 0;
+for (int i = 1; i <= x; i++)
+{
+    y = Math.Pow(i,3);
+    Console.Write("\t" + y);
+}*/
+Console.WriteLine("Введите число ");
+double n = double.Parse(Console.ReadLine());
+double y = 0;
+double i = 1;
+if (n < 0)
+{
+    i = n;
+    n = -1;
+}
+for (; i <= n; i++)
+{
+    y = Math.Pow(i,3);
+    Console.Write("\t" + y);
+}
